369c10e89ca3020699d887066c47017fcd0171c3
Florin Salasan <florinsalasan@gmail.com>
2024-02-11 17:30:41 -0500

    tried out some of the example code in the discordgo repo, reverted back to before

--- streaks.go
+++ streaks.go
@@ -6,14 +6,18 @@ import (
     "os"
     "os/signal"
     "time"
+    "strings"
+    "errors"
 
     "github.com/joho/godotenv"
 
     "github.com/bwmarrin/discordgo"
 )
 
+var discSess *discordgo.Session
+
 func main() {
-    dotenverr := godotenv.Load()
+    dotenverr := godotenv.Load(".env")
     if dotenverr != nil {
         log.Fatal("Error loading .env file")
     }
@@ -21,6 +25,9 @@ func main() {
     APP_ID := os.Getenv("APP_ID")
     PUBLIC_KEY := os.Getenv("PUBLIC_KEY")
     DISCORD_TOKEN := os.Getenv("DISCORD_TOKEN")
+    CHANNEL_ID := os.Getenv("CHANNEL_ID")
+    GUILD_ID := os.Getenv("GUILD_ID")
+    RemoveCommands := true
 
     discord, _ := discordgo.New("Bot " + DISCORD_TOKEN)
     discord.AddHandler(func(discord *discordgo.Session, r *discordgo.Ready) {
@@ -33,14 +40,17 @@ func main() {
     }
     defer discord.Close()
 
-    event := createEvent(discord)
+    // event := createEvent(discord)
+
+    fmt.Printf("APP_ID: %v\n", APP_ID)
+    fmt.Printf("PUBLIC_KEY: %v\n", PUBLIC_KEY)
+    fmt.Printf("DISCORD_TOKEN: %v\n", DISCORD_TOKEN)
+    fmt.Printf("CHANNEL_ID: %v\n", CHANNEL_ID)
+    fmt.Printf("GUILD_ID: %v\n", GUILD_ID)
 
     stop := make(chan os.Signal, 1)
     signal.Notify(stop, os.Interrupt)
     <-stop
     log.Println("Shutdown")
 
-}
-
-func createEvent(discord *discordgo.Session) {
 }
